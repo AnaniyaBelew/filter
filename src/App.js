c7a235abd4d8fefc7f29dfeb03047a83b0a48673
Ananiya <anewscho@gmail.com>
2023-08-10 18:40:53 +0300

    optimized

--- src/App.js
+++ src/App.js
@@ -15,19 +15,21 @@ class App extends Component {
     .then((response)=>response.json())
     .then((users)=>this.setState(()=>{return {People:users}},()=>console.log(this.state.People)));
   }
+  onSearchChange=(event)=>
+  {
+      console.log(event.target.value)
+      const searchstr=event.target.value.toLowerCase();
+      this.setState(()=>{return{searchstr};
+    });
+  };
 render(){
   const filteredPeople= this.state.People.filter((people)=>
   {
         return (people.name.toLowerCase().includes(this.state.searchstr))
-  });
+  })
   return (
     <div className="App">
-      <input className="search-box" type="search" placeholder="Search people" onChange={(event)=>
-        {
-            console.log(event.target.value)
-            const searchstr=event.target.value.toLowerCase();
-            this.setState(()=>{return{searchstr}})
-        }}/>
+      <input className="search-box" type="search" placeholder="Search people" onChange={this.onSearchChange}/>
       {
         filteredPeople.map((People)=>
         {
